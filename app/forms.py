61e64903d95670180760432626dc7a426beb3250
François Terrier <fterrier@gmail.com>
2019-01-17 11:51:37 +0100

    added email validation and 404 page

--- app/forms.py
+++ app/forms.py
@@ -5,4 +5,4 @@ class JobAdForm(FlaskForm):
     texttotest = TextAreaField(u'', [validators.Length(min=1)])
     name = StringField(u'Name', [validators.required()])
     company = StringField(u'Company', [validators.required()])
-    email = StringField(u'Email', [validators.required()])
+    email = StringField(u'Email', [validators.required(), validators.Email()])
